e98b828ac41250d59609c3fd3f4aa7ac2c147452
Lindsey Tam <lindseytam@gmail.com>
2019-12-12 21:46:37 -0800

    writing

--- Meskin.m
+++ Meskin.m
@@ -1,4 +1,5 @@
 initialStateGuess = [4;1;3;4]; % xhat[k|k-1]
+%initialStateGuess = [1;4;2;5]; % bad intials
 
 % Construct the filter
 ukf = unscentedKalmanFilter(...
@@ -6,7 +7,9 @@ ukf = unscentedKalmanFilter(...
     @MeskinMeasurementNonAddFcn,... % Measurement function
     initialStateGuess,...
     'HasAdditiveMeasurementNoise',false)%,...
-    %'alpha', 0.9);
+    %'alpha', 0.9,...
+    %'kappa', 3,...
+    %'beta', 2);
     
 R = .001; % Variance of the measurement noise v[k]
 ukf.MeasurementNoise = R;
@@ -20,20 +23,14 @@ timeVector = 0:T:5;
 rng(1); % Fix the random number generator for reproducible results
 yTrue = xTrue(:,1);
 yMeas = yTrue + (sqrt(R)*randn(size(yTrue))); 
-    % sqrt(R): Standard deviation of noise
-    % randn(size(yTrue)): randomly sample 51 elements from ytrue
-%yMeas = yTrue;
-
-%Nsteps = numel(yMeas); % Number of time steps
-%xCorrectedUKF = zeros(Nsteps,4); % Corrected state estimates
-%PCorrected = zeros(Nsteps,4,4); % Corrected state estimation error covariances
-%e = zeros(Nsteps,1); % Residuals (or innovations)
+% sqrt(R): Standard deviation of noise
+% randn(size(yTrue)): randomly sample 51 elements from ytrue
+%yMeas = yTrue; Assumes no noise
 
 for k=1:numel(yMeas) 
     % Let k denote the current time.
     
     % Residuals (or innovations): Measured output - Predicted output
-    
     %e(k) = yMeas(k) - MeskinMeasurementFcn(ukf.State); % ukf.State is x[k|k-1] at this point
     
     % Incorporate the measurements at time k into the state estimates by
@@ -41,23 +38,7 @@ for k=1:numel(yMeas)
     % properties of the filter to contain x[k|k] and P[k|k]. These values
     % are also produced as the output of the "correct" command.
     
-    %[xCorrectedUKF(k,:), PCorrected(k,:,:)] = correct(ukf, yMeas(k));
     [xCorrectedUKF(k,:), PCorrected(k,:,:)] = correct(ukf, yMeas(k));
-    %[xCorrectedUKF(k,:,:), PCorrected(k,:,:,:)] = correct(ukf, yMeas(k, 2));
-    %for j=1:numel(yMeas) 
-    %[xCorrectedUKF(k,:), PCorrected(k,k,:)] = correct(ukf, yMeas(j, 1));
-    %[xCorrectedUKF(k+1,:), PCorrected(k+1,:,:)] = correct(ukf, yMeas(j, 2));
-    %[xCorrectedUKF(k+2,:), PCorrected(k+2,:,:)] = correct(ukf, yMeas(j, 3));
-    %[xCorrectedUKF(k+3,:), PCorrected(k+3,:,:)] = correct(ukf, yMeas(j, 4));
-    %end
-    
-    %xCorrectedUKF(:,1) = first col
-    %xCorrectedUKF(1,:) = first row
-    %xCorrectedUKF(k,:) = kth row row
-    %PCorrected(k,:,:) = first row of each of the 4 matrices
-    
-    %There are 4 51x4 state covariances
-    
     
     % Predict the states at next time step, k+1. This updates the State and
     % StateCovariance properties of the filter to contain x[k+1|k] and
@@ -68,47 +49,109 @@ end
 
 
 figure();
+color1=[0,0.4470, 0.7410]; %blue
+color2=[0.4660 0.6740 0.1880]; %green
+color3=[0.8500 0.3250 0.0980]; %orange
+color4=[0.9290 0.6940 0.1250]; %yellow
 
-subplot(2,3,1);
-plot(timeVector,xTrue(:,1), '-b',timeVector,xCorrectedUKF(:,1), '--b',...
-    timeVector,yMeas(:), '*b',...
-    'LineWidth', 3);
-set(gca, 'FontSize', 20);
-legend('True','UKF estimate','Measured')
+subplot(2,2,1);
+plot(timeVector, xTrue(:,1), 'Color', color1, 'LineStyle', '-',...
+    'LineWidth', 1)
+hold on 
+plot(timeVector,xCorrectedUKF(:,1), 'Color', color1, 'LineStyle', '--',...
+    'LineWidth', 1)
+hold on
+plot(timeVector,yMeas, 'Color', color1, 'Marker', '.',...
+    'LineWidth', 1)
+
+set(gca, 'FontSize', 15);
+set(gcf, 'Color', 'None');
+a = legend('True','UKF estimate','Measured')
+a.FontSize = 10;
 ylim([0 5]);
-ylabel('x_1', 'FontSize', 20);
+ylabel('x_1', 'FontSize', 15);
+xlabel('Time [s]', 'FontSize', 15);
 
-subplot(2,3,2);
-plot(timeVector,xTrue(:,2),'-g', timeVector,xCorrectedUKF(:,2), '--g',...
-    'LineWidth', 3);
-set(gca, 'FontSize', 20);
+subplot(2,2,2);
+plot(timeVector, xTrue(:,2), 'Color', color2, 'LineStyle', '-',...
+    'LineWidth', 1)
+hold on 
+plot(timeVector,xCorrectedUKF(:,2), 'Color', color2, 'LineStyle', '--',...
+    'LineWidth', 1)
+set(gca, 'FontSize', 15);
+b = legend('True','UKF estimate')
+b.FontSize = 10;
 ylim([0 5]);
-xlabel('Time [s]', 'FontSize', 20);
-ylabel('x_2', 'FontSize', 20);
+xlabel('Time [s]', 'FontSize', 15);
+ylabel('x_2', 'FontSize', 15);
+xlabel('Time [s]', 'FontSize', 15);
 
-subplot(2,3,3);
-plot(timeVector,xTrue(:,3), 'r',timeVector,xCorrectedUKF(:,3), '--r',...
-    'LineWidth', 3);
-set(gca, 'FontSize', 20);
-legend('True','UKF estimate','Measured')
+subplot(2,2,3);
+plot(timeVector, xTrue(:,3), 'Color', color3, 'LineStyle', '-',...
+    'LineWidth', 1)
+hold on 
+plot(timeVector,xCorrectedUKF(:,3), 'Color', color3, 'LineStyle', '--',...
+    'LineWidth', 1)
+set(gca, 'FontSize', 15);
+c = legend('True','UKF estimate')
+c.FontSize = 10;
 %ylim([-2.6 2.6]);
-ylabel('x_3', 'FontSize', 20);
+ylabel('x_3', 'FontSize', 15);
+xlabel('Time [s]', 'FontSize', 15);
 
-subplot(2,3,4);
-plot(timeVector,xTrue(:,4), 'c',timeVector,xCorrectedUKF(:,4), '--c',...
-   'LineWidth', 3);
-set(gca, 'FontSize', 20);
-legend('True','UKF estimate','Measured')
+subplot(2,2,4);
+plot(timeVector, xTrue(:,4), 'Color', color4, 'LineStyle', '-',...
+    'LineWidth', 1)
+hold on 
+plot(timeVector,xCorrectedUKF(:,4), 'Color', color4, 'LineStyle', '--',...
+    'LineWidth', 1)
+set(gca, 'FontSize', 15);
+d = legend('True','UKF estimate','Measured')
+d.FontSize = 10;
 %ylim([-2.6 2.6]);
-ylabel('x_4', 'FontSize', 20);
+ylabel('x_4', 'FontSize', 15);
+xlabel('Time [s]', 'FontSize', 15);
 
-subplot(2,3,5);
-plot(timeVector,xTrue(:,1), '-b',timeVector,xCorrectedUKF(:,1), '--b',...
-    timeVector,xTrue(:,2), '-g',timeVector,xCorrectedUKF(:,2), '--g',...
-    timeVector,xTrue(:,3), '-r',timeVector,xCorrectedUKF(:,3), '--r',...
-    timeVector,xTrue(:,4), '-c',timeVector,xCorrectedUKF(:,4), '--c',...
-   'LineWidth', 3);
-set(gca, 'FontSize', 20);
-legend('x1 True','x1 UKF','x2 True', 'x2 UKF', 'x3 True','x3 UKF','x4 True', 'x4 UKF')
+%saveas(gcf,'\Users\lindseytam\Desktop\thesis\Meskin_states_badIntial.png')
+%saveas(gcf,'\Users\lindseytam\Desktop\thesis\Meskin_states.png')
+
+subplot(1,1,1);
+
+plot(timeVector, xTrue(:,1), 'Color', color1, 'LineStyle', '-',...
+    'LineWidth', 1)
+hold on 
+plot(timeVector,xCorrectedUKF(:,1), 'Color', color1, 'LineStyle', '--',...
+    'LineWidth', 1)
+hold on
+plot(timeVector,yMeas, 'Color', color1, 'Marker', '.',...
+    'LineWidth', 1)
+hold on
+plot(timeVector, xTrue(:,2), 'Color', color2, 'LineStyle', '-',...
+    'LineWidth', 1)
+hold on 
+plot(timeVector,xCorrectedUKF(:,2), 'Color', color2, 'LineStyle', '--',...
+    'LineWidth', 1)
+hold on
+plot(timeVector, xTrue(:,3), 'Color', color3, 'LineStyle', '-',...
+    'LineWidth', 1)
+hold on 
+plot(timeVector,xCorrectedUKF(:,3), 'Color', color3, 'LineStyle', '--',...
+    'LineWidth', 1)
+hold on
+plot(timeVector, xTrue(:,4), 'Color', color4, 'LineStyle', '-',...
+    'LineWidth', 1)
+hold on 
+plot(timeVector,xCorrectedUKF(:,4), 'Color', color4, 'LineStyle', '--',...
+    'LineWidth', 1)
+
+set(gca, 'FontSize', 15);
+set(gcf, 'color', 'none');
+set(gca, 'color', 'none');
+e = legend('x1 True','x1 UKF','x2 True', 'x2 UKF', 'x3 True','x3 UKF','x4 True', 'x4 UKF')
+e.FontSize = 10;
 ylim([0 5]);
-ylabel('x_4', 'FontSize', 20);
+ylabel('x_4', 'FontSize', 15);
+xlabel('Time [s]', 'FontSize', 15);
+
+%saveas(gcf,'\Users\lindseytam\Desktop\thesis\Meskin_overall_badIntial.png')
+saveas(gcf,'\Users\lindseytam\Desktop\thesis\Meskin_overall.png')
